3675e1c40f1efac713c593c0b8372d7ff9becd6e
karthik <karthik5112001@gmail.com>
2025-06-08 17:04:59 +0530

    check

--- package/react/utils/src/utils.tsx
+++ package/react/utils/src/utils.tsx
@@ -1,9 +1,11 @@
 export type formatDateProps = {
   label?: string;
 };
-  
 
-export const gehl = "asdfa                                                ";
+
+export const gehl = "asdfa                                             ";
+
+
 
 export function formatDate({ label = "Date" }: formatDateProps = {}): string {
   const date = new Date().toDateString();
